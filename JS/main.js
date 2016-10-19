537298daf5867e2d12875fbc044e9ddf284b5ad1
Carmen Chico <CarmenChico@icloud.com>
2016-10-19 16:56:55 -0400

    accordionized image

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,3 @@
-// ---we use this at the begining so that the document waits for everything to be present and ready before it starts working....
-//this maybe applies to the src being a link instead of the downloaded file we have acquired. (need to check)
-
-// jQuery(document).ready(functions($){
-
-// }
- $(title).click(function(){
-//
-// }
 
 //close everthing up
 //the first thing we did was to set it up to close ... we did that through the scss
@@ -24,33 +15,44 @@
 
 
 //now that we were able to get it to close  by applying the overflow to the id, we have to create a function to close it based on the click area.
-var event = document.addEventListener("click", function() {
-var $this = $(this);
+ // var event = document.addEventListener("click", function() {
+// var $this = $(this);
 //we want to test to see if this works.
-  console.log(document.getElementById("click"));
-});
+  // console.log(document.getElementById("click"));
+
+
+// });
 
 function closePara(event) {
-document.getElementById("para1").on("click")
+  console.log(event);
 
-  if (==="#para1") {
+  if (event.target.id==="header1") {
+    console.log("Hey");
     $("#para1").removeClass("hidePara");
-  }
+    $("#para2").addClass("hidePara");
+    $("#para3").addClass("hidePara");
 
-  // $("#para2").removeClass("hidePara");
+    }
 
-  // $("#para3").removeClass("hidePara");
+    if (event.target.id==="header2") {
+      console.log("Hey");
+      $("#para2").removeClass("hidePara");
+      $("#para1").addClass("hidePara");
+      $("#para3").addClass("hidePara");
+    }
+    if (event.target.id==="header3") {
+      console.log("Hey");
+      $("#para3").removeClass("hidePara");
+      $("#para1").addClass("hidePara");
+      $("#para2").addClass("hidePara");
+    }
 
 }
-
-
-
 // Shows all div content
-closePara();
+// closePara(event);
+// document.getElementById("para1").addEventListener("click")
+$(".title").click(closePara);
 
-
-
-//
 
 
 
